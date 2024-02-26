7d78a35903c60d2475658b57cb26535a4c7d7eca
snehatiwari972 <154247453+snehatiwari972@users.noreply.github.com>
2024-02-26 12:08:08 +0530

    Update mail.js

--- login_OTP/modules/model/mail.js
+++ login_OTP/modules/model/mail.js
@@ -8,15 +8,15 @@ async function config(toMail, subject,text){
    const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: "snehatiwari972@gmail.com",
-            pass: "fkarhrshclskjweo"
+            user: "", // G-Mail Id 
+            pass: "" // your password
         }
     })
 
     // configure email content 
 
     const mailOptions = {
-        from :"snehatiwari972@gmail.com",
+        from :"", // G-Mail Id 
         to : toMail,
         subject: subject,
         text: text
@@ -30,11 +30,6 @@ async function config(toMail, subject,text){
     }
 }
 
-// sendMail();
+
 module.exports = config;
 
-// const config = {
-// emailUser: 'snehatiwari972@gmail.com',
-// emailPassword: 'fkarhrshclskjweo'
-// }
-// module.exports = config;
